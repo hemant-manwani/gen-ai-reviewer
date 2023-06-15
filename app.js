16d914d344fe959cc0423d66150a00e6d4bd9793
Hemant <hemantmanwani.iitr@gmail.com>
2023-06-15 20:52:12 +0530

    working code

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import { createNodeMiddleware } from '@octokit/webhooks';
 import fs from 'fs';
 import http from 'http';
 
+import robot from './robot.js';
+
 dotenv.config();
 
 const appId = process.env.APP_ID;
@@ -20,39 +22,6 @@ const app = new App({
   },
 });
 
-const messageForNewPRs = 'Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.';
-
-async function handlePullRequestOpened({ octokit, payload }) {
-  console.log(`Received a pull request event for #${payload.pull_request.number}`);
-
-  try {
-    await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      issue_number: payload.pull_request.number,
-      body: messageForNewPRs,
-      headers: {
-        "x-github-api-version": "2022-11-28",
-      },
-    });
-  } catch (error) {
-    if (error.response) {
-      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
-    }
-    console.error(error)
-  }
-};
-
-app.webhooks.on("pull_request.opened", handlePullRequestOpened);
-
-app.webhooks.onError((error) => {
-  if (error.name === 'AggregateError') {
-    console.error(`Error processing request: ${error.event}`);
-  } else {
-    console.error(error);
-  }
-});
-
 const port = 3000;
 const host = 'localhost';
 const path = '/api/webhook';
@@ -63,6 +32,7 @@ const middleware = createNodeMiddleware(app.webhooks, {path});
 http.createServer(middleware).listen(port, () => {
   console.log(`Server is listening for events at: ${localWebhookUrl}`);
   console.log('Press Ctrl + C to quit.')
+  robot(app);
 });
 
 try {
